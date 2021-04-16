7ef23bf1e711908e58231887cb66c9b3144a369c
Alexander Fok <foktmalexander@gmail.com>
2021-04-15 23:06:07 -0700

    Add API call for deletion and get saved books

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 
 const searchFor = (keyword) => axios.get(`/api/search?title=${keyword}`);
 
-const saveBook = (props) => axios.post('/api/saveBook', props)
+const saveBook = (props) => axios.post('/api/book', props);
+
+const deleteBook = (id) => axios.delete('/api/book', {data: {id: id}});
+
+const getBooks = () => axios.get('/api/books');
 
 const API = {
-  searchFor, saveBook
+  searchFor,
+  saveBook,
+  deleteBook,
+  getBooks
 }
 
 export default API;
