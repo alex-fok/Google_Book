bd23da80ccc3f8f971e5ddc1324b6e65c45fc269
Alexander Fok <foktmalexander@gmail.com>
2021-04-15 23:02:38 -0700

    Add Saved Page

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,5 +1,43 @@
+import { useState, useEffect } from 'react';
+import BookList from '../../components/BookList';
+import API from '../../utils/API';
+import WarningMessage from '../../components/WarningMessage';
+
 const Save = () => {
-  return <div></div>
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    const run = async () => {
+      const {data} = await API.getBooks();
+      setBookList(data)
+    }
+    run();
+  }, []);
+
+  const del = {
+    type: 'Delete',
+    theme: 'outline-danger',
+    fn: async (props) => {
+      try {
+        await API.deleteBook(props._id);
+        setBookList(prev => prev.slice().filter(book => book._id !== props._id));
+      } catch(err) {
+        console.log('Error: Cannot perform deletion');
+      }
+    }
+  }
+
+  return (
+    <div>
+      { bookList.length
+        ? <BookList
+            books={bookList}
+            btn={del}
+          />
+        : <WarningMessage msg='No Books Found!' />
+      }
+    </div>
+  )
 }
 
 export default Save;
