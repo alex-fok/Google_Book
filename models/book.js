ef83a44ef57d1bbf9deaa15ccd17dec7b66bbb9d
Alexander Fok <foktmalexander@gmail.com>
2021-04-15 17:36:51 -0700

    Redesign database for book

--- models/book.js
+++ models/book.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+// id, title, snippet, authors, image
 const bookSchema = new Schema({
   _id: {type: String, required: true},
   title: { type: String, required: true },
+  snippet: {type: String},
   authors: { type: [String] },
+  image: {type: String},
   date: { type: Date, default: Date.now }
 });
 
